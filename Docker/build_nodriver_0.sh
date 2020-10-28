4664763df27bb241365901be3cb42cba740e1bae
Dave Fisk <dfisk@nvidia.com>
2020-10-28 16:29:07 -0700

    Refined wgst docker support

--- Docker/build_nodriver_0.sh
+++ Docker/build_nodriver_0.sh
@@ -1,2 +1,5 @@
 cp ../build/wgst software/
-docker build -f Dockerfile_nodriver_0 -t wgst-nodriver .
+docker build -f Dockerfile_nodriver_0 -t wgst:1.1 .
+echo example: last argument is loop count passed to entery point:
+echo docker run -i --gpus device=0 --device=/dev/nvidiactl --device=/dev/nvidia-uvm wgst:1.1 10
+
