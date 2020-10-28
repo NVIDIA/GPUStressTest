33d92b98987fc5bdc2551ac8fcc5b7d345013fde
Dave Fisk <dfisk@nvidia.com>
2020-10-28 16:53:51 -0700

    Add NGC repo to wgst image tag

--- Docker/build_nodriver_0.sh
+++ Docker/build_nodriver_0.sh
@@ -1,5 +1,5 @@
 cp ../build/wgst software/
-docker build -f Dockerfile_nodriver_0 -t wgst:1.1 .
+docker build -f Dockerfile_nodriver_0 -t nvcr.io/nvidian/wgst:1.1 .
 echo example: last argument is loop count passed to entery point:
 echo docker run -i --gpus device=0 --device=/dev/nvidiactl --device=/dev/nvidia-uvm wgst:1.1 10
 
