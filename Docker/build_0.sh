17b0cb129d5365884b50b325dea22400e8988377
Dave Fisk <dfisk@nvidia.com>
2022-05-18 16:19:39 -0700

    Update docker support

--- Docker/build_0.sh
+++ Docker/build_0.sh
@@ -1,2 +1,3 @@
+./populate_software.sh
 cp ../build/gst software/
-docker build -f Dockerfile_0 -t gst-ubuntu-18.04 .
+docker build -f Dockerfile_0 -t us-west2-docker.pkg.dev/egx-anthos-gpu-operator-val/gst/v2:ubuntu-18.04 .
--- Docker/populate_software.sh
+++ Docker/populate_software.sh
@@ -1,6 +1,7 @@
 cp /usr/local/cuda/targets/x86_64-linux/lib/libcublasLt.so.11 ./software/
 cp /usr/local/cuda/targets/x86_64-linux/lib/libcudart.so ./software/
 cp /usr/lib/x86_64-linux-gnu/libcuda.so.1 ./software/
+cp /usr/lib/x86_64-linux-gnu/libcuda.so.1 ./software/
 
 
 
